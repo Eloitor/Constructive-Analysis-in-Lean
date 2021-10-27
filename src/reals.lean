2a4ed0e5c4e812e731035cbc4d88f049db846d06
Eloi Torrents <eloitor@disroot.org>
2021-10-27 15:46:58 +0200

    real_equivalent_iff: right implication

--- src/reals.lean
+++ src/reals.lean
@@ -17,28 +17,82 @@ lemma real_equivalent_symm {a b: ℕ → ℚ} (h_a: regular a) (h_b: regular b) 
     specialize h_eq n h_n,
     have : - (b n - a n) = (a n - b n),
       by simp [h_eq],
-    rw [←abs_neg, this],
-    assumption,
+    rwa [←abs_neg, this],
   end
+
+/-instance jaja : ordered_semiring ℕ :=
+  begin
+    exact nat.ordered_semiring,
+  end-/
 
 lemma real_equivalent_iff {a b: ℕ → ℚ} (h_a: regular a) (h_b: regular b): 
     (a ∼ b) ↔ (∀ (j: ℕ), 0 < j → ∃ N, ∀ n ≥ N, | a n - b n | ≤ (j: ℚ)⁻¹) :=
   begin
     split,
-    { intros h_eq h_j h_n,
-      use 2 * h_j,
-      intros h_k h_l,
+    { intros h_eq j h_j_pos,
+      use 2 * j,
+      intros n h_n_ge_two_j,
       unfold equivalent at h_eq,
-      specialize h_eq h_k _,
+      specialize h_eq n _,
       {
-        calc 0 < h_j : h_n
-          ... ≤ 2*h_j : by { rw le_mul_iff_one_le_left, exact one_le_two, exact h_n }
-          ... ≤ h_k : h_l,
+        calc 0 < j : h_j_pos
+          ... ≤ 2*j : by { rw le_mul_iff_one_le_left, exact one_le_two, assumption }
+          ... ≤ n : h_n_ge_two_j,
       },
-      calc |a h_k - b h_k| ≤ | a h_k | + | b h_k | : by {apply abs_sub,}
-        ... ≤ (h_j : ℚ)⁻¹ : by sorry,
+      have : (n : ℚ)⁻¹ ≤ (2*j : ℚ)⁻¹,
+      {
+        have h2j : 0 < 2 * j,
+          {
+            exact nat.succ_mul_pos 1 h_j_pos,
+          },
+        rw le_inv,
+        {
+          simp*,
+          obtain := λ x, rat.of_int (int.of_nat x),
+          have : ∀ (a b : ℕ), a ≤ b → (a : ℚ) ≤ (b : ℚ),
+          {
+            intros a b h,
+            exact nat.cast_le.mpr h
+          },
+          suffices t : 2 * j ≤ n,
+          {
+            obtain tt := nat.cast_le.mpr h_n_ge_two_j,
+            norm_num at tt,
+            exact tt,
+            exact rat.nontrivial,
+          },
+          exact h_n_ge_two_j,
+        },
+        {
+          simp*,
+          exact gt_of_ge_of_gt h_n_ge_two_j h2j,
+        },
+        {
+          simpa,
+        },
+      },
+      have : (n : ℚ)⁻¹ * 2 ≤ (j : ℚ)⁻¹,
+      {
+        have t : (n : ℚ)⁻¹ * 2 ≤ (2*j : ℚ)⁻¹ * 2,
+        {
+          simp only [this, mul_le_mul_right, zero_lt_bit0, zero_lt_one],
+        },
+        have tt : (2*j : ℚ)⁻¹ * 2 = (j : ℚ)⁻¹,
+        {
+          have m : (2*j : ℚ)⁻¹ = 2⁻¹ * (j : ℚ)⁻¹,
+          {
+            exact mul_inv₀
+          },
+          rw [m],
+          ring,
+        },
+        rwa ← tt,
+      },
+      exact le_trans h_eq this,
     },
     { 
+     -- cases h_a with ha_regular,
+     -- cases h_b with hb_regular,
       sorry,
     },
   end
